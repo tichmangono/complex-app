78b70c70dd29d73b762d73c5294050164a7daa04
Tichakunda Mangono <tichmangono@gmail.com>
2020-12-23 23:26:17 -0500

    :building_construction: controllers for MVC

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+exports.login = function (req, res) {}
+
+exports.logout = function (req, res) {}
+
+exports.register = function (req, res) {}
+
+exports.home = function (req, res) {
+  res.render("home-guest")
+}
