2e7eadc77409a634b33e18ad6567dada828e5b8a
Tichakunda Mangono <tichmangono@gmail.com>
2020-12-25 22:01:34 -0500

    :white_check_mark: email validate, string cleanup

--- models/User.js
+++ models/User.js
@@ -6,6 +6,25 @@ let User = function (data) {
   this.errors = []
 }
 
+User.prototype.cleanUp = function () {
+  if (typeof this.data.username != "string") {
+    this.data.username = ""
+  }
+  if (typeof this.data.email != "string") {
+    this.data.email = ""
+  }
+  if (typeof this.data.password != "string") {
+    this.data.password = ""
+  }
+
+  // get rid of any bogus props
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password.trim(),
+  }
+}
+
 User.prototype.validate = function () {
   if (this.data.username == "") {
     this.errors.push("You must provide a username")
@@ -35,6 +54,7 @@ User.prototype.validate = function () {
 
 User.prototype.register = function () {
   //Step #1: Validate user data
+  this.cleanUp()
   this.validate()
   //Step #2: only if there are no validation errors
   // then save user data into a database
