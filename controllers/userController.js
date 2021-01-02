0cb9d866b6e10bbf8e1df032baa51b2807b6031f
Tichakunda Mangono <tichmangono@gmail.com>
2021-01-01 21:40:59 -0500

    :hammer: user login using Promises, then, catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,20 @@ const User = require("../models/User")
 
 exports.login = function (req, res) {
   let user = new User(req.body)
-  user.login(function (result) {
-    res.send(result)
-  })
+  user
+    .login()
+    .then(function (result) {
+      res.send(result)
+    })
+    .catch(function (err) {
+      res.send(err)
+    })
 }
 
 exports.logout = function (req, res) {}
 
 exports.register = function (req, res) {
-  user = new User(req.body)
+  let user = new User(req.body)
   user.register()
   if (user.errors.length > 0) {
     res.send(user.errors)
