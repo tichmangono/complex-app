ebcba638abc342e37a3bd46f14b4ba9c5bce9dc9
Tichakunda Mangono <tichmangono@gmail.com>
2021-01-05 22:25:24 -0500

    🎨 post model and controller to mongodb

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,17 @@
-const User = require("../models/User")
+const Post = require("../models/Post")
 
 exports.viewCreateScreen = function (req, res) {
   res.render("create-post")
+}
+
+exports.create = function (req, res) {
+  let post = new Post(req.body)
+  post
+    .create()
+    .then(function () {
+      res.send("New post created")
+    })
+    .catch(function (errors) {
+      res.send(errors)
+    })
 }
