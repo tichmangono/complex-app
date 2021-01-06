487eb1d326f1a5ddad8f83af402a0b2c630b5db8
Tichakunda Mangono <tichmangono@gmail.com>
2021-01-05 23:12:11 -0500

    🔨 view single post, pull from db

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,4 @@
+const { resolveInclude } = require("ejs")
 const Post = require("../models/Post")
 
 exports.viewCreateScreen = function (req, res) {
@@ -16,6 +17,11 @@ exports.create = function (req, res) {
     })
 }
 
-exports.viewSingle = function (req, res) {
-  res.render("single-post-screen")
+exports.viewSingle = async function (req, res) {
+  try {
+    let post = await Post.findSingleById(req.params.id)
+    res.render("single-post-screen", { post: post })
+  } catch {
+    res.send("404 template will go here.")
+  }
 }
